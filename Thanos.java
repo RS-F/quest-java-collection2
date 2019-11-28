4ecd19172158091ba8e9e597f594d34c565f90f3
RS-F <rsf.sea@gmail.com>
2019-11-28 15:32:18 +0100

    solution found

--- Thanos.java
+++ Thanos.java
@@ -1,32 +1,45 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Thanos {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        List<Hero> heroes = new ArrayList<>();
-        heroes.add(new Hero("Hulk", 49));
-        heroes.add(new Hero("Black Widow", 34));
-        heroes.add(new Hero("Captain America", 100));
-        heroes.add(new Hero("Thor", 1501));
+		List<Hero> heroes = new ArrayList<>();
+		heroes.add(new Hero("Hulk", 49));
+		heroes.add(new Hero("Black Widow", 34));
+		heroes.add(new Hero("Captain America", 100));
+		heroes.add(new Hero("Thor", 1501));
 
-        // TODO 1 : Modify Hero to implements Comparable and sort by name (ascending)
+		// TODO 1 : Modify Hero to implements Comparable and sort by name (ascending)
+		Collections.sort(heroes);
 
+		System.out.println("\nOrder by name:");
+		showList(heroes);
 
-        System.out.println("\nOrder by name:");
-        showList(heroes);
+		// TODO 2: Add a Comparator and sort by age (descending)
 
-        // TODO 2: Add a Comparator and sort by age (descending)
+		boolean reverseOrder = true;
 
+		class SortbyAge implements Comparator<Hero> {
+			public int compare(Hero h1, Hero h2) {
+				Integer i1 = h1.getAge();
+				Integer i2 = h2.getAge();
+				if (reverseOrder)
+					return i2.compareTo(i1);
+				else
+					return i1.compareTo(i2);
+			}
+		}
 
-        System.out.println("\nOrder by age:");
-        showList(heroes);
-    }
+		Collections.sort(heroes, new SortbyAge());
 
-    private static void showList(List<Hero> heroes) {
-        for (Hero heroe : heroes) {
-            System.out.println(heroe.getName() + ", " + heroe.getAge());
-        }
-    }
+		System.out.println("\nOrder by age:");
+		showList(heroes);
+	}
+
+	private static void showList(List<Hero> heroes) {
+		for (Hero heroe : heroes) {
+			System.out.println(heroe.getName() + ", " + heroe.getAge());
+		}
+	}
 }
